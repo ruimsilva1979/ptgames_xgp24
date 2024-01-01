cd8a26dd5cb3f6543a07288e12c325561701ed7f
Rui Silva <ptgamespontopt@gmail.com>
2024-01-01 12:09:59 +0000

    - Instalção projecto.

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+defined('DB_HOST') ? null : define('DB_HOST', 'localhost');
+defined('DB_USER') ? null : define('DB_USER', 'xgproyect');
+defined('DB_PASS') ? null : define('DB_PASS', 'xgproyect');
+defined('DB_NAME') ? null : define('DB_NAME', 'xgproyect');
+defined('DB_PREFIX') ? null : define('DB_PREFIX', 'xgp_');
+defined('SECRETWORD') ? null : define('SECRETWORD', 'xgp-lMONodalWiXQBIcl');
+?>
